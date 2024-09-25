1c54c84594408b3e260cbc33e801c4a00aef73b0
IzabelaMilisiewiczKainos <izabela.milisiewicz@kainos.com>
2024-09-25 21:20:13 +0200

    adding async await

--- test/specs/home.js
+++ test/specs/home.js
@@ -2,11 +2,18 @@
 //https://practice.sdetunicorns.com/
 
 describe('Home', () => {
-    it('Open URL & assert title', () => {
+    it('Open URL & assert title', async () => {
         //Open URL
-        browser.url('https://practice.sdetunicorns.com/');
+        await browser.url('https://practice.sdetunicorns.com/');
 
         //Assert Title
-        expect(browser).toHaveTitle('Practice E-Commerce Site – SDET Unicorns');
+        await expect(browser).toHaveTitle('Practice E-Commerce Site – SDET Unicorns');
     })
 });
+
+
+//first parameter of describe - Test suite name
+//second parameter of describe - function (function will have all tests ("it" blocks)
+
+//first arg of it - test case name
+//second arg for it is function (function will have test steps)
--- wdio.conf.js
+++ wdio.conf.js
@@ -64,7 +64,7 @@ export const config = {
     // Define all options that are relevant for the WebdriverIO instance here
     //
     // Level of logging verbosity: trace | debug | info | warn | error | silent
-    logLevel: 'info',
+    logLevel: 'debug',
     //
     // Set specific log levels per logger
     // loggers:
@@ -88,7 +88,7 @@ export const config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    // baseUrl: 'http://localhost:8080',
+    baseUrl: 'https://practice.sdetunicorns.com',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
