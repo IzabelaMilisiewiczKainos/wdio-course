41770530210550dd0f0fe8fc6d01c134c71ef1ee
IzabelaMilisiewiczKainos <izabela.milisiewicz@kainos.com>
2024-10-07 20:42:14 +0200

    upload in hidden input field

--- test/specs/upload.js
+++ test/specs/upload.js
@@ -9,7 +9,7 @@ describe("Upload Test", () => {
     await browser.url("https://the-internet.herokuapp.com/upload");
   });
 
-  it("Simple upload test", async () => {
+  it.skip("Simple upload test", async () => {
     // Resolve the file path
     const filePath = path.join(__dirname, "../", "data", "workday-logo.svg"); //__dirname is the current directory, '../..' is the parent directory, 'data' is the directory where the file is located,
 
@@ -22,5 +22,43 @@ describe("Upload Test", () => {
 
     //assertion
     await expect($("h3")).toHaveText("File Uploaded!"); //assertion to check if the file is uploaded successfully
+  });
+
+  it("Upload on a hidden input field", async () => {
+    //open url
+    await browser.url("/cart");
+
+    await browser.execute(() => {
+      console.log(
+        ".........................moj consssssole loooooog: " +
+          document.querySelector("#upfile_1"),
+      );
+    });
+
+    // Resolve the file path
+    const filePath = path.join(__dirname, "../", "data", "picture.jpg");
+
+    // Upload the file
+    const remoteFilePath = await browser.uploadFile(filePath);
+
+    // Check the element in the browser context
+    await browser.execute(() => {
+      // This function allows you to run code inside the browser context, where the document object is available (HTML DOM). By using browser.execute(), we ensure that our JavaScript code (which interacts with the DOM) is running in the browser, and hence can safely interact with the DOM.
+      const element = document.querySelector("#upfile_1");
+      console.log(element);
+      if (element) {
+        //if condition is used to ensure that the element exists on the page before attempting to manipulate it. This is a common defensive programming technique to avoid errors like undefined or null references.
+        element.className = ""; // Remove hidden class
+      }
+    });
+
+    //set file path value in the input field
+    await $("#upfile_1").setValue(remoteFilePath);
+    await $("#upload_1").click();
+
+    //assertion
+    await expect($("#wfu_messageblock_header_1_1")).toHaveText(
+      "File picture.jpg uploaded successfully",
+    ); //assertion to check if the file is uploaded successfully
   });
 });
